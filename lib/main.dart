3b6bbdc78610f74037c9965bf27d7f7395cf853a
deniafrian252 <deniafrian252@gmail.com>
2023-09-18 10:41:19 +0700

    flexible widger

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,49 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("Latihan Animated Container")),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {});
-            },
-            child: AnimatedContainer(
-              duration: const Duration(seconds: 1),
-              width: 50.0 + random.nextInt(101),
-              height: 50.0 + random.nextInt(101),
-              color: Color.fromARGB(255, random.nextInt(257),
-                  random.nextInt(257), random.nextInt(257)),
-            ),
-          ),
-        ),
+        appBar: AppBar(title: const Text("Flexibel Layout")),
+        body: Column(children: [
+          Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.pink,
+                        margin: EdgeInsets.all(10),
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.purple,
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.green,
+                      )),
+                ],
+              )),
+          Flexible(
+              flex: 2,
+              child: Container(
+                color: Colors.amber,
+              )),
+          Flexible(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+              )),
+        ]),
       ),
     );
   }
