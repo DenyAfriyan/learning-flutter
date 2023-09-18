3588e9879ae7155f0e3439448c8542b6123645c5
deniafrian252 <deniafrian252@gmail.com>
2023-09-18 11:34:27 +0700

    stack align

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,135 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("Flexibel Layout")),
-        body: Column(children: [
-          Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.pink,
-                        margin: EdgeInsets.all(10),
-                      )),
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.purple,
-                      )),
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        color: Colors.green,
-                      )),
-                ],
-              )),
-          Flexible(
-              flex: 2,
-              child: Container(
-                color: Colors.amber,
-              )),
-          Flexible(
-              flex: 1,
-              child: Container(
-                color: Colors.blue,
-              )),
-        ]),
+        appBar: AppBar(title: Text("Belajar Stack dan Align")),
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            )),
+                      ],
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black12,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            )),
+                      ],
+                    ))
+              ],
+            ),
+            ListView(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Text(
+                        "Ini Adalah Text yang ada di lapisan tengah stack",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment(0.9, 0.9),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.amber, // Background color
+                ),
+                child: Text("My Button"),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
