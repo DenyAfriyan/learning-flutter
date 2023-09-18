14db8b1c8a1d528819f8d2c7e754edaf024a1491
deniafrian252 <deniafrian252@gmail.com>
2023-09-18 13:00:22 +0700

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,24 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Latihan Image'),
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 200,
-            padding: EdgeInsets.all(3),
-            child: Image(
-              image: AssetImage('images/stone.jpg'),
-              fit: BoxFit.contain,
-              repeat: ImageRepeat.repeat,
-            ),
-          ),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:first_app/second_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Main Page")),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return SecondPage();
+                }));
+              },
+              child: Text("Go To Second Page"))),
+    );
+  }
+}
